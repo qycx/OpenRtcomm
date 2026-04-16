c746e957fa2147e8a24ed1558b82dd2a757fb44b
yuhaizhou123 <2861085227@qq.com>
2026-04-16 15:41:09 +0800

    代码提交

---
+++ README.md
@@ -0,0 +1,4 @@
+## openRtComm-new
+
+
+
---
+++ tmpInclude/myinttypes.h
@@ -0,0 +1,13 @@
+
+#ifndef  __myinttypes_h__
+#define __myinttypes_h__  // {
+
+
+#define PRIx8     "hhx"
+#define PRIx16    "hx"
+#define PRIx32    "lx"
+#define PRIx64    "llx"
+
+#endif  //  }
+
+
