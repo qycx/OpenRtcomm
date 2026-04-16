0cae4892d21ac9b4d11e6efc5a326109baf5f4be
yuhaizhou123 <2861085227@qq.com>
2026-04-16 12:44:55 +0800

    清空

--- myBindingClassLibrary_ffmpeg/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,4 +0,0 @@
-using System;
-
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.qycx.tmpffmpeglibrary", Managed="Com.Qycx.Tmpffmpeglibrary")]
-
--- myBindingClassLibrary_tmpJavaLib/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,4 +0,0 @@
-using System;
-
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.qycx.tmpjavalibrary", Managed="Com.Qycx.Tmpjavalibrary")]
-
